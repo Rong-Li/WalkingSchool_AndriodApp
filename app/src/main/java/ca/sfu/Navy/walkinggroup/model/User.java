7eecba6fa3bb1e7e3d80751508d9412a41ae57c1
kfhg <32785234+kfhg@users.noreply.github.com>
2018-03-10 23:53:17 -0800

    Fixed commit

--- app/src/main/java/ca/sfu/Navy/walkinggroup/model/User.java
+++ app/src/main/java/ca/sfu/Navy/walkinggroup/model/User.java
@@ -23,7 +23,9 @@ public class User {
     public String getName() {
         return name;
     }
-    public String getEmail() {return email;}
+    public String getEmail() {
+        return email;
+    }
     public String getPassword() {
         return password;
     }
